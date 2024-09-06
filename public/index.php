8c34bf3af199a35a22d7fcea8b726e536ec09902
jaylordibe <jaylordibe@gmail.com>
2024-09-06 09:59:05 +0800

    Initial project

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>API</title>
-    </head>
-    <body>
-        <h1><?php echo 'Welcome to API!'; ?></h1>
-    </body>
-</html>
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
